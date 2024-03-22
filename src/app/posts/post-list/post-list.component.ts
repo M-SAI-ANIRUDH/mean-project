282f8a34d7fea043131cb62744d34c8f3240b506
M-SAI-ANIRUDH <anirudhmsai@gmail.com>
2024-03-23 00:01:30 +0530

    Added nodeJS expressJS as BE

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {NgIf, NgFor} from '@angular/common';
 import { postData } from './../../models/post-data'
-import { postService } from '../../services/post-call-api-service';
+import { postService } from '../../services/post-call-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [MatExpansionModule, NgFor, NgIf],
+  imports: [MatExpansionModule, NgFor, NgIf, HttpClientModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
@@ -18,7 +19,8 @@ export class PostListComponent implements OnInit{
 
   constructor(public postService : postService){}
   ngOnInit(): void {
-    this.postService.getPosts()
+    this.postService.getPosts();
+    this.postService.getPostsSubject()
       .subscribe((res)=>{
         this.posts = res
       })
